0695ebcc53c5ceba07442df43bc58ef51eb37630
anomius <swarajpurohit@gmail.com>
2021-10-26 18:15:49 +0000

    added a folder structure

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
     var video = document.getElementById('video');
     var source = document.getElementById('source');
     if (Math.random() * 2 > 1) {
-        source.setAttribute('src', "a.mp4");
+        source.setAttribute('src', "stuff/vid/a.mp4");
         console.log("a.mp4");
     } else {
-        source.setAttribute('src', "b.mp4");
+        source.setAttribute('src', "stuff/vid/b.mp4");
         console.log("b.mp4");
     }
 
